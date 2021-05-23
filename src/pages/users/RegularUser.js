c9792ee65875a325770ff595ffa72807f4d3622d
Yunxuan Yu <yunxuany@andrew.cmu.edu>
2021-05-23 21:00:09 +0800

    add commits

--- src/pages/users/RegularUser.js
+++ src/pages/users/RegularUser.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { getToken } from '../../utils/auth'
+import { ifExpire } from '../../services/ifExpire'
 
 
 function RegularUser() {
+    console.log(ifExpire())
     return (
         <div>
             <h1>RegularUser</h1>
